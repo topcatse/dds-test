7dbec1149e8275faf391305f377b83c5f639aaba
Tommy Carlsson <tc@infobric.se>
2018-03-07 16:53:52 +0100

    Client/server ok. Added config

--- K8s/client.py
+++ K8s/client.py
@@ -1,6 +1,13 @@
 import socket
 import sys
 import getopt
+import time
+import logging
+import pickle
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+logger = logging.getLogger('')
+logger.setLevel(logging.INFO)
 
 buffer_size = 4096
 
@@ -9,45 +16,60 @@ def main(argv):
     port = 10001
     server_address = 'localhost'
     server_port = 10000
+    sleep_s = 1
+    id = 0
     
     try:
-        opts, args = getopt.getopt(argv,"hc:p:a:s:",["count=", "port=", "serveraddress=", "serverport="])
+        opts, args = getopt.getopt(argv,"hc:p:a:s:i:",["count=", "port=", "serveraddress=", "serverport=", "id=", "sleep=", "log="])
     except getopt.GetoptError:
-        print('client.py -c <count> -p <port> -a <serveraddress> -s <serverport>')
+        print('client.py -c <count> -p <port> -a <serveraddress> -s <serverport> --id=<id> --sleep=<secs> --log=<debug|...>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('client.py -c <count> -p <port> -a <serveraddress> -s <serverport>')
+            print('client.py -c <count> -p <port> -a <serveraddress> -s <serverport> --id=<id> --sleep=<secs> --log=<debug|...>')
             sys.exit()
         elif opt in ("-c", "--count"):
             count = int(arg)
         elif opt in ("-p", "--port"):
-            port = arg
+            port = int(arg)
         elif opt in ("-a", "--serveraddress"):
             server_address = arg
         elif opt in ("-s", "--serverport"):
-            server_port = arg
+            server_port = int(arg)
+        elif opt in ("-i", "--id"):
+            id = int(arg)
+        elif opt in ("--sleep"):
+            sleep_s = int(arg)
+        elif opt in ("--log"):
+            numeric_level = getattr(logging, arg.upper(), None)
+            if not isinstance(numeric_level, int):
+                raise ValueError('Invalid log level: %s' % arg)
+            logger.setLevel(level=numeric_level)            
 
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     sock.bind(('localhost', port))
 
+    info = [id, 0, port]
+    
     try:    
         for c in list(range(count)):
-            message =  'This is message {}'.format(c)
-            print('Sending "{}"'.format(message))
-            sent = server_sock.sendto(message.encode(), (server_address, server_port))
+            info[1] = c
+            logger.info('Sending {}'.format(info))
+            sent = server_sock.sendto(pickle.dumps(info), (server_address, server_port))
 
-            print('waiting to receive')
+            logger.debug('Waiting to receive')
             data, server = sock.recvfrom(buffer_size)
-            print('Received {} bytes from {}'.format(len(data), server))
+            logger.debug('Received {} bytes from {}'.format(len(data), server))
 
             if data:
-                print('With content: "{}"'.format(data.decode()))
+                logger.info('Received: {}'.format(pickle.loads(data)))
+
+            time.sleep(sleep_s)
 
     finally:
-        print('Closing sockets')
+        logger.debug('Closing sockets')
         server_sock.close()
         sock.close()    
 
--- K8s/server.py
+++ K8s/server.py
@@ -1,7 +1,12 @@
 import socket
 import sys
 import getopt
+import logging
+import pickle
 
+logging.basicConfig(format='%(asctime)s %(message)s')
+logger = logging.getLogger('')
+            
 buffer_size = 4096
 
 def main(argv):
@@ -9,34 +14,39 @@ def main(argv):
     client_port = 10001
 
     try:
-        opts, args = getopt.getopt(argv,"hc:p:",["clientport=", "port="])
+        opts, args = getopt.getopt(argv,"hp:",["port=", "log="])
     except getopt.GetoptError:
-        print('server.py -c <clientport> -p <port>')
+        print('server.py -p <port> --log=<debug|...>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('server.py -c <clientport> -p <port>')
+            print('server.py -p <port> --log=<debug|...>')
             sys.exit()
-        elif opt in ("-c", "--clientport"):
-            client_port = arg
         elif opt in ("-p", "--port"):
-            port = arg
+            port = int(arg)
+        elif opt in ("--log"):
+            numeric_level = getattr(logging, arg.upper(), None)
+            if not isinstance(numeric_level, int):
+                raise ValueError('Invalid log level: %s' % arg)
+            logger.setLevel(numeric_level)                        
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_sock    = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    sock.bind(('localhost', 10000))
+    sock.bind(('localhost', port))
 
     while True:
-        print('Waiting to receive message')
+        logger.debug('Waiting to receive message')
         data, address = sock.recvfrom(buffer_size)
     
-        print('Received {} bytes from {}'.format(len(data), address))
+        logger.debug('Received {} bytes from {}'.format(len(data), address))
     
         if data:
-            print('With content: {}'.format(data.decode()))
-            sent = client_sock.sendto(data, (address[0], client_port))
-            print('Sent {} bytes back to {}'.format(sent, address))
+            info = pickle.loads(data)
+            logger.info('Received: {}'.format(info))
+            client_address = (address[0], info[2])
+            sent = client_sock.sendto(data, client_address)
+            logger.debug('Sent {} bytes back to {}'.format(sent, client_address))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
